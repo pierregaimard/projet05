9b7d7c8ca7fc86d6863c8c75b5d4b840f723266c
Pierre Gaimard <pierre@gaimard-family.fr>
2020-10-28 17:05:08 +0100

    Add front controller file

---
+++ public/index.php
@@ -0,0 +1,30 @@
+<?php
+
+    // autoload
+
+    require_once __DIR__ . "/../vendor/autoload.php";
+
+    // Error Handeler
+
+    // Todo: Delete Error handeler when blog is in production.
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+
+    // Use
+
+    use Climb\Controller\FrontController;
+    use Climb\Exception\AppException;
+
+    // Session
+
+    session_start();
+
+    // Front controller
+
+try {
+    $frontController = new FrontController();
+    $response = $frontController->getResponse();
+    $response->send();
+} catch (AppException $exception) {
+    echo $exception;
+}
