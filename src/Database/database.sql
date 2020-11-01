18fb03f32c6175430a1edabc74bbbf7e657417c4
Pierre Gaimard <pierre@gaimard-family.fr>
2020-11-01 15:29:47 +0100

    Fix code style in database.sql file

--- src/Database/database.sql
+++ src/Database/database.sql
@@ -16,9 +16,9 @@ SET lc_time_names = 'fr_FR';
 -- Table user_status --
 -- ----------------- --
 CREATE TABLE user_status (
-    `id`     TINYINT      UNSIGNED NOT NULL AUTO_INCREMENT,
-    `status` VARCHAR(10),
-    PRIMARY KEY (`id`)
+    id     TINYINT      UNSIGNED NOT NULL AUTO_INCREMENT,
+    status VARCHAR(10),
+    PRIMARY KEY (id)
 )
     ENGINE=INNODB
     DEFAULT CHARSET=utf8mb4;
@@ -28,17 +28,17 @@ CREATE TABLE user_status (
 -- Table user --
 -- ---------- --
 CREATE TABLE user (
-    `id`              SMALLINT         UNSIGNED NOT NULL AUTO_INCREMENT,
-    `first_name`      VARCHAR(30)      NOT NULL,
-    `last_name`       VARCHAR(30)      NOT NULL,
-    `email`           VARCHAR(50)      NOT NULL,
-    `password`        VARCHAR(120)     NOT NULL,
-    `id_user_status`  TINYINT UNSIGNED NOT NULL,
-    `last_login_date` DATE,
-    `bad_credentials` TINYINT          UNSIGNED,
-    PRIMARY KEY (`id`),
-    CONSTRAINT `fk_user_id_user_status` FOREIGN KEY (`id_user_status`) REFERENCES `user_status` (`id`),
-    CONSTRAINT UNIQUE INDEX `ind_uni_email` (`email`) -- user email is used for login so it must be unique
+    id              SMALLINT         UNSIGNED NOT NULL AUTO_INCREMENT,
+    first_name      VARCHAR(30)      NOT NULL,
+    last_name       VARCHAR(30)      NOT NULL,
+    email           VARCHAR(50)      NOT NULL,
+    password        VARCHAR(120)     NOT NULL,
+    id_user_status  TINYINT UNSIGNED NOT NULL,
+    last_login_date DATE,
+    bad_credentials TINYINT          UNSIGNED,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_user_id_user_status FOREIGN KEY (id_user_status) REFERENCES user_status (id),
+    CONSTRAINT UNIQUE INDEX ind_uni_email (email) -- user email is used for login so it must be unique
 )
     ENGINE=INNODB
     DEFAULT CHARSET=utf8mb4;
@@ -48,9 +48,9 @@ CREATE TABLE user (
 -- Table user_role --
 -- --------------- --
 CREATE TABLE user_role (
-    `id`   TINYINT    UNSIGNED NOT NULL AUTO_INCREMENT,
-    `role` VARCHAR(6) NOT NULL,
-    PRIMARY KEY (`id`)
+    id   TINYINT    UNSIGNED NOT NULL AUTO_INCREMENT,
+    role VARCHAR(6) NOT NULL,
+    PRIMARY KEY (id)
 )
     ENGINE=INNODB
     DEFAULT CHARSET=utf8mb4;
@@ -60,11 +60,11 @@ CREATE TABLE user_role (
 -- Table as_user_role --
 -- ------------------ --
 CREATE TABLE as_user_role (
-    `id_user`      SMALLINT UNSIGNED NOT NULL,
-    `id_user_role` TINYINT  UNSIGNED NOT NULL,
-    PRIMARY KEY (`id_user`, `id_user_role`),
-    CONSTRAINT `fk_as_user_role_id_user`      FOREIGN KEY (`id_user`)      REFERENCES `user` (`id`)      ON DELETE CASCADE,
-    CONSTRAINT `fk_as_user_role_id_user_role` FOREIGN KEY (`id_user_role`) REFERENCES `user_role` (`id`)
+    id_user      SMALLINT UNSIGNED NOT NULL,
+    id_user_role TINYINT  UNSIGNED NOT NULL,
+    PRIMARY KEY (id_user, id_user_role),
+    CONSTRAINT fk_as_user_role_id_user      FOREIGN KEY (id_user)      REFERENCES user (id)      ON DELETE CASCADE,
+    CONSTRAINT fk_as_user_role_id_user_role FOREIGN KEY (id_user_role) REFERENCES user_role (id)
 )
     ENGINE=INNODB,
     DEFAULT CHARSET=utf8mb4;
@@ -74,15 +74,15 @@ CREATE TABLE as_user_role (
 -- Table blog_post --
 -- --------------- --
 CREATE TABLE blog_post (
-    `id`               SMALLINT     UNSIGNED NOT NULL AUTO_INCREMENT,
-    `title`            VARCHAR(50)  NOT NULL,
-    `chapo`            VARCHAR(100) NOT NULL,
-    `content`          TEXT         NOT NULL,
-    `creation_time`    DATETIME     NOT NULL,
-    `last_update_time` DATETIME     NOT NULL,
-    `id_user`          SMALLINT     UNSIGNED NOT NULL,
-    PRIMARY KEY (`id`),
-    CONSTRAINT `fk_blog_post_id_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE
+    id               SMALLINT     UNSIGNED NOT NULL AUTO_INCREMENT,
+    title            VARCHAR(50)  NOT NULL,
+    chapo            VARCHAR(100) NOT NULL,
+    content          TEXT         NOT NULL,
+    creation_time    DATETIME     NOT NULL,
+    last_update_time DATETIME     NOT NULL,
+    id_user          SMALLINT     UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_blog_post_id_user FOREIGN KEY (id_user) REFERENCES user (id) ON DELETE CASCADE
 )
     ENGINE=INNODB,
     DEFAULT CHARSET=utf8mb4;
@@ -92,9 +92,9 @@ CREATE TABLE blog_post (
 -- Table blog_post_comment_status --
 -- ------------------------------ --
 CREATE TABLE blog_post_comment_status (
-    `id`     TINYINT     UNSIGNED NOT NULL AUTO_INCREMENT,
-    `status` VARCHAR(10),
-    PRIMARY KEY (`id`)
+    id     TINYINT     UNSIGNED NOT NULL AUTO_INCREMENT,
+    status VARCHAR(10),
+    PRIMARY KEY (id)
 )
     ENGINE=INNODB
     DEFAULT CHARSET=utf8mb4;
@@ -104,16 +104,16 @@ CREATE TABLE blog_post_comment_status (
 -- Table blog_post_comment --
 -- ----------------------- --
 CREATE TABLE blog_post_comment (
-    `id`                          INT      UNSIGNED NOT NULL AUTO_INCREMENT,
-    `comment`                     TINYTEXT NOT NULL,
-    `time`                        DATETIME NOT NULL,
-    `id_blog_post`                SMALLINT UNSIGNED NOT NULL,
-    `id_user`                     SMALLINT UNSIGNED NOT NULL,
-    `id_blog_post_comment_status` TINYINT  UNSIGNED NOT NULL,
-    PRIMARY KEY (`id`),
-    CONSTRAINT `fk_blog_post_comment_id_blog_post`                FOREIGN KEY (`id_blog_post`)                REFERENCES `blog_post` (`id`)                ON DELETE CASCADE,
-    CONSTRAINT `fk_blog_post_comment_id_user`                     FOREIGN KEY (`id_user`)                     REFERENCES `user` (`id`)                     ON DELETE CASCADE,
-    CONSTRAINT `fk_blog_post_comment_id_blog_post_comment_status` FOREIGN KEY (`id_blog_post_comment_status`) REFERENCES `blog_post_comment_status` (`id`)
+    id                          INT      UNSIGNED NOT NULL AUTO_INCREMENT,
+    comment                     TINYTEXT NOT NULL,
+    time                        DATETIME NOT NULL,
+    id_blog_post                SMALLINT UNSIGNED NOT NULL,
+    id_user                     SMALLINT UNSIGNED NOT NULL,
+    id_blog_post_comment_status TINYINT  UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_blog_post_comment_id_blog_post                FOREIGN KEY (id_blog_post)                REFERENCES blog_post (id)                ON DELETE CASCADE,
+    CONSTRAINT fk_blog_post_comment_id_user                     FOREIGN KEY (id_user)                     REFERENCES user (id)                     ON DELETE CASCADE,
+    CONSTRAINT fk_blog_post_comment_id_blog_post_comment_status FOREIGN KEY (id_blog_post_comment_status) REFERENCES blog_post_comment_status (id)
 )
     ENGINE=INNODB,
     DEFAULT CHARSET=utf8mb4;
@@ -131,16 +131,16 @@ DELIMITER |
 -- blog_post triggers --
 -- ------------------ --
 -- BEFORE INSERT --
-CREATE TRIGGER `before_insert_blog_post` BEFORE INSERT
-ON `blog_post` FOR EACH ROW
+CREATE TRIGGER before_insert_blog_post BEFORE INSERT
+ON blog_post FOR EACH ROW
 BEGIN
     SET NEW.creation_time = NOW();    -- set blog_post creation time to now on insertion --
     SET NEW.last_update_time = NOW(); -- set blog_post last update time to now on insertion --
 END |
 
 -- BEFORE UPDATE --
-CREATE TRIGGER `before_update_blog_post` BEFORE UPDATE
-ON `blog_post` FOR EACH ROW
+CREATE TRIGGER before_update_blog_post BEFORE UPDATE
+ON blog_post FOR EACH ROW
 BEGIN
     SET NEW.last_update_time = NOW(); -- update last update time to now when updating blog post --
 END |
@@ -149,8 +149,8 @@ END |
 -- blog_post_comment_trigger --
 -- ------------------------- --
 -- BEFORE INSERT --
-CREATE TRIGGER `before_insert_blog_post_comment` BEFORE INSERT
-ON `blog_post_comment` FOR EACH ROW
+CREATE TRIGGER before_insert_blog_post_comment BEFORE INSERT
+ON blog_post_comment FOR EACH ROW
 BEGIN
     SET NEW.time = NOW(); -- set blog post comment insertion time to now on insertion --
 END |
@@ -167,7 +167,7 @@ DELIMITER ;
 -- -------------------- --
 -- user_status fixtures --
 -- -------------------- --
-INSERT INTO `user_status`
+INSERT INTO user_status
     (status)
 VALUES
     ('VALIDATION'), ('ACTIVE'), ('LOCKED');
@@ -175,7 +175,7 @@ VALUES
 -- ------------------ --
 -- user_role fixtures --
 -- ------------------ --
-INSERT INTO `user_role`
+INSERT INTO user_role
     (role)
 VALUES
     ('ADMIN'), ('MEMBER');
@@ -183,7 +183,7 @@ VALUES
 -- -------------------------- --
 -- blog_post_comment_fixtures --
 -- -------------------------- --
-INSERT INTO `blog_post_comment_status`
+INSERT INTO blog_post_comment_status
     (status)
 VALUES
     ('VALIDATION'), ('APPROVED');
