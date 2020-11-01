8203fce62afb24f1b7226db278138e594c11c2b6
Pierre Gaimard <pierre@gaimard-family.fr>
2020-11-01 20:26:56 +0100

    Add UserStatus entity class

---
+++ src/Model/Entity/UserStatus.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Model\Entity;
+
+/**
+ * @Table(name="user_status")
+ */
+class UserStatus
+{
+    /**
+     * @var int
+     *
+     * @Column(name="id")
+     */
+    private int $key;
+
+    /**
+     * @var string
+     *
+     * @Column(name="status")
+     */
+    private string $status;
+
+    /**
+     * @return int
+     */
+    public function getKey(): int
+    {
+        return $this->key;
+    }
+
+    /**
+     * @param int $key
+     */
+    public function setKey(int $key): void
+    {
+        $this->key = $key;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param string $status
+     */
+    public function setStatus(string $status): void
+    {
+        $this->status = $status;
+    }
+}
