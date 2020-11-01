ee951caee070cee861aea2559da98f4e217038e2
Pierre Gaimard <pierre@gaimard-family.fr>
2020-11-01 20:28:33 +0100

    Add UserRole entity class

---
+++ src/Model/Entity/UserRole.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Model\Entity;
+
+/**
+ * @Table(name="user_role")
+ */
+class UserRole
+{
+    /**
+     * @var int
+     *
+     * @Column(name="id")
+     */
+    private int $key;
+
+    /**
+     * @var string
+     *
+     * @Column(name="role")
+     */
+    private string $role;
+
+    /**
+     * @return int
+     */
+    public function getKey(): int
+    {
+        return $this->key;
+    }
+
+    /**
+     * @param int $key
+     */
+    public function setKey(int $key): void
+    {
+        $this->key = $key;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRole(): string
+    {
+        return $this->role;
+    }
+
+    /**
+     * @param string $role
+     */
+    public function setRole(string $role): void
+    {
+        $this->role = $role;
+    }
+}
