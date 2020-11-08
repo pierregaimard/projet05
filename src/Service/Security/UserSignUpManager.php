67aa2f511d03622c25782acec7967e5bc993cec5
Pierre Gaimard <pierre@gaimard-family.fr>
2020-11-08 20:55:48 +0100

    Add methods to UserSignUpManager service

--- src/Service/Security/UserSignUpManager.php
+++ src/Service/Security/UserSignUpManager.php
@@ -3,31 +3,64 @@
 namespace App\Service\Security;
 
 use App\Model\Entity\User;
+use App\Model\Entity\UserRole;
+use App\Model\Entity\UserStatus;
+use App\Service\Email\EmailManager;
+use Climb\Http\Session\SessionInterface;
 use Climb\Orm\EntityManager;
 use Climb\Orm\Orm;
 use Climb\Exception\AppException;
+use Climb\Security\UserPasswordManager;
 
 class UserSignUpManager
 {
+    private const TEMP_USER_KEY = 'tempUser';
+
     /**
      * @var EntityManager
      */
     private EntityManager $entityManager;
 
     /**
-     * @param Orm $orm
+     * @var SessionInterface
+     */
+    private SessionInterface $session;
+
+    /**
+     * @var UserPasswordManager
+     */
+    private UserPasswordManager $passwordManager;
+
+    /**
+     * @var EmailManager
+     */
+    private EmailManager $emailManager;
+
+    /**
+     * @param Orm                 $orm
+     * @param SessionInterface    $session
+     * @param UserPasswordManager $passwordManager
+     * @param EmailManager        $emailManager
      *
      * @throws AppException
      */
-    public function __construct(Orm $orm)
-    {
-        $this->entityManager = $orm->getManager('App');
+    public function __construct(
+        Orm $orm,
+        SessionInterface $session,
+        UserPasswordManager $passwordManager,
+        EmailManager $emailManager
+    ) {
+        $this->entityManager   = $orm->getManager('App');
+        $this->session         = $session;
+        $this->passwordManager = $passwordManager;
+        $this->emailManager    = $emailManager;
     }
 
     /**
      * @param string $email
      *
      * @return string[]|true
+     *
      * @throws AppException
      */
     public function checkUserDuplication(string $email)
@@ -44,5 +77,55 @@ class UserSignUpManager
             'message' => 'An account width this email address already exists. Please try with an other one or ' .
                 'uses "forgot password" functionality.'
         ];
+    }
+
+    /**
+     * @param array $tempUserData
+     */
+    public function setTempUser(array $tempUserData): void
+    {
+        $this->session->add(self::TEMP_USER_KEY, $tempUserData);
+    }
+
+    /**
+     * @return array
+     */
+    public function getTempUser(): array
+    {
+        return $this->session->get(self::TEMP_USER_KEY);
+    }
+
+    public function unsetTempUser(): void
+    {
+        $this->session->remove(self::TEMP_USER_KEY);
+    }
+
+    /**
+     * @param string $password
+     *
+     * @return User
+     *
+     * @throws AppException
+     */
+    public function setFinalUser(string $password): User
+    {
+        $statusRepository = $this->entityManager->getRepository(UserStatus::class);
+        $status = $statusRepository->findOneBy(['status' => User::STATUS_VALIDATION]);
+
+        $roleRepository = $this->entityManager->getRepository(UserRole::class);
+        $role = $roleRepository->findOneBy(['role' => User::ROLE_MEMBER]);
+
+        $tempUserData = $this->getTempUser();
+        $user = new User();
+        $user->setFirstName($tempUserData['firstName']);
+        $user->setLastName($tempUserData['lastName']);
+        $user->setEmail($tempUserData['email']);
+        $user->setPassword($this->passwordManager->getPasswordHash($password));
+        $user->setStatus($status);
+        $user->addRole($role);
+
+        $this->entityManager->insertOne($user);
+
+        return $user;
     }
 }
