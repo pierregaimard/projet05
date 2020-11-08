54bac57531bed428bcd6705629a0f686f0e9ad8b
Pierre Gaimard <pierre@gaimard-family.fr>
2020-11-08 10:54:13 +0100

    Add MinLengthDataChecker in form feature

---
+++ src/Service/Form/DataChecker/MinLengthDataChecker.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Service\Form\DataChecker;
+
+class MinLengthDataChecker
+{
+    /**
+     * @param string   $data
+     * @param int|null $length
+     *
+     * @return string|true
+     */
+    public function check(string $data, int $length = null)
+    {
+        if ($length === null || strlen($data) > $length) {
+            return true;
+        }
+
+        return 'This field requires minimum ' . $length . ' characters long';
+    }
+}
