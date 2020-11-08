0b4763ef79f173193e3a31b256b54428b83ab06d
Pierre Gaimard <pierre@gaimard-family.fr>
2020-11-08 21:57:46 +0100

    Add Users relation to UserRole entity

--- src/Model/Entity/UserRole.php
+++ src/Model/Entity/UserRole.php
@@ -2,6 +2,8 @@
 
 namespace App\Model\Entity;
 
+use Climb\Orm\EntityBag;
+
 /**
  * @Table(name="user_role")
  */
@@ -20,6 +22,18 @@ class UserRole
      * @Column(name="role")
      */
     private string $role;
+
+    /**
+     * @var EntityBag
+     *
+     * @Relation(type="association", entity="App\Model\Entity\User", association="as_user_role", invertedBy="roles")
+     */
+    private EntityBag $users;
+
+    public function __construct()
+    {
+        $this->users = new EntityBag();
+    }
 
     /**
      * @return int
@@ -51,5 +65,37 @@ class UserRole
     public function setRole(string $role): void
     {
         $this->role = $role;
+    }
+
+    /**
+     * @return User[]
+     */
+    public function getUsers()
+    {
+        return $this->users->getAll();
+    }
+
+    /**
+     * @param array $users
+     */
+    public function setUsers(array $users): void
+    {
+        $this->users->setAll($users);
+    }
+
+    /**
+     * @param User $user
+     */
+    public function addUser(User $user): void
+    {
+        $this->users->add($user->getKey(), $user);
+    }
+
+    /**
+     * @param User $user
+     */
+    public function removeUser(User $user): void
+    {
+        $this->users->remove($user->getKey());
     }
 }
