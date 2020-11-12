04202df399e595cca50d7815391c7260a876266b
Pierre Gaimard <pierre@gaimard-family.fr>
2020-11-12 22:25:10 +0100

    Add findValidatedMembers method to UserRepository

--- src/Model/Repository/UserRepository.php
+++ src/Model/Repository/UserRepository.php
@@ -2,8 +2,28 @@
 
 namespace App\Model\Repository;
 
+use App\Model\Entity\User;
 use Climb\Orm\EntityRepository;
+use Climb\Exception\AppException;
 
 class UserRepository extends EntityRepository
 {
+    /**
+     * @return User[]|null
+     *
+     * @throws AppException
+     */
+    public function findValidatedMembers()
+    {
+        $request = "
+            SELECT * FROM user
+            INNER JOIN as_user_role ON user.id = as_user_role.id_user AND as_user_role.id_user_role = :user_role
+            WHERE user.id_status != :user_status
+        ";
+
+        return $this->findByRequest(
+            $request,
+            ['user_role' => 2, 'user_status' => 1]
+        );
+    }
 }
