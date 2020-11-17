c10937284bf27e349cbd846e5033f44f533e30bb
Pierre Gaimard <pierre@gaimard-family.fr>
2020-11-17 08:16:14 +0100

    Add finfByRole method to UserRepository class

--- src/Model/Repository/UserRepository.php
+++ src/Model/Repository/UserRepository.php
@@ -27,4 +27,26 @@ class UserRepository extends EntityRepository
             ['user_role' => 2, 'user_status' => 1]
         );
     }
+
+    /**
+     * @param string $role
+     *
+     * @return User[]|null
+     *
+     * @throws AppException
+     */
+    public function findByRole(string $role)
+    {
+        $request = "
+            SELECT * FROM user
+            INNER JOIN as_user_role ON user.id = as_user_role.id_user 
+            INNER JOIN user_role ON as_user_role.id_user_role = user_role.id AND user_role.role = :role
+            ORDER BY user.first_name ASC, user.last_name ASC 
+        ";
+
+        return $this->findByRequest(
+            $request,
+            ['role' => $role]
+        );
+    }
 }
