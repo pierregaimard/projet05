deb6447f87ce7ba972a4728a066ca9ddd9896c5f
Pierre Gaimard <pierre@gaimard-family.fr>
2020-11-17 08:34:49 +0100

    Fix code style in blog_post_add.js file

--- public/assets/js/blog_post_add.js
+++ public/assets/js/blog_post_add.js
@@ -1,21 +1,18 @@
 ClassicEditor
-    .create( document.querySelector( '#content' ), {
-        toolbar: [ 'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'fontColor','|', 'indent', 'outdent', 'alignment', '|', 'blockQuote', 'insertTable', 'code', 'codeBlock', '|', 'undo', 'redo' ],
+    .create( document.querySelector( "#content" ), {
+        toolbar: [ "heading", "|", "bold", "italic", "link", "bulletedList", "numberedList", "fontColor", "|", "indent", "outdent", "alignment", "|", "blockQuote", "insertTable", "code", "codeBlock", "|", "undo", "redo" ],
         heading: {
             options: [
-                { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
-                { model: 'heading1', view: 'h2', title: 'Heading 1', class: 'ck-heading_heading1' },
-                { model: 'heading2', view: 'h3', title: 'Heading 2', class: 'ck-heading_heading2' },
-                { model: 'heading3', view: 'h4', title: 'Heading 3', class: 'ck-heading_heading3' },
-                { model: 'heading4', view: 'h5', title: 'Heading 4', class: 'ck-heading_heading4' }
+                { model: "paragraph", title: "Paragraph", class: "ck-heading_paragraph" },
+                { model: "heading1", view: "h2", title: "Heading 1", class: "ck-heading_heading1" },
+                { model: "heading2", view: "h3", title: "Heading 2", class: "ck-heading_heading2" },
+                { model: "heading3", view: "h4", title: "Heading 3", class: "ck-heading_heading3" },
+                { model: "heading4", view: "h5", title: "Heading 4", class: "ck-heading_heading4" }
             ]
         },
         fontColor: {
             colors: [
-                { color: '#e9490f', label: 'Orange' }
+                { color: "#e9490f", label: "Orange" }
             ]
         }
-    })
-    .catch( error => {
-        console.error( error );
     });
