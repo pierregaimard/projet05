2fc531d0bb6b0a910c91a396767cab3cff61e38a
Pierre Gaimard <pierre@gaimard-family.fr>
2020-11-22 16:13:02 +0100

    Add InitializationController controller class

---
+++ src/Controller/Init/InitializationController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller\Init;
+
+use App\Service\Init\InitializationManager;
+use Climb\Controller\AbstractController;
+use Climb\Exception\AppException;
+use Climb\Http\RedirectResponse;
+use Climb\Routing\Annotation\Route;
+
+class InitializationController extends AbstractController
+{
+    /**
+     * @var InitializationManager
+     */
+    private InitializationManager $initManager;
+
+    public function __construct(InitializationManager $initManager)
+    {
+        $this->initManager = $initManager;
+    }
+
+    /**
+     * @Route(path="/initialize", name="initialize")
+     *
+     * @throws AppException
+     */
+    public function initialize()
+    {
+        if ($this->initManager->hasProjectInit()) {
+            return $this->redirectToRoute('home');
+        }
+
+        $this->initManager->initializeDatabase();
+        $this->initManager->initAdminUser();
+        $this->initManager->setUser();
+
+        $response = new RedirectResponse(
+            $this->getRoutePath('account_home'),
+            $this->initManager->getData()
+        );
+        $response->getFlashes()->add(
+            'message',
+            [
+                'status' => 'success',
+                'message' => '<b>Blog has been successfully initialized!</b><br>' .
+                    'Please set your admin account informations.'
+            ]
+        );
+
+        return $response;
+    }
+}
