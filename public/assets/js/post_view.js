44c5ac32944eb132867af17e35f92820bcce9e40
Pierre Gaimard <pierre@gaimard-family.fr>
2020-11-25 15:16:48 +0100

    Fix double quotes problem in post_view.js file

--- public/assets/js/post_view.js
+++ public/assets/js/post_view.js
@@ -3,19 +3,19 @@
 
 hljs.initHighlightingOnLoad();
 $(document).ready(function () {
-    let $action = $('.app-comment-actions a');
-    let $confirmText = $('#post-comment-action-confirm .app-confirm-action');
-    let $confirmAction = $('#post-comment-action-confirm a');
+    let $action = $(".app-comment-actions a");
+    let $confirmText = $("#post-comment-action-confirm .app-confirm-action");
+    let $confirmAction = $("#post-comment-action-confirm a");
 
-    $action.on('click', function () {
-        let text = $(this).data('text');
-        let actionText = $(this).data('action');
-        let actionHref = $(this).data('action-href');
-        let actionClass = $(this).data('action-class');
+    $action.on("click", function () {
+        let text = $(this).data("text");
+        let actionText = $(this).data("action");
+        let actionHref = $(this).data("action-href");
+        let actionClass = $(this).data("action-class");
 
         $confirmText.html(text);
-        $confirmAction.attr('href', actionHref);
-        $confirmAction.attr('class', actionClass);
+        $confirmAction.attr("href", actionHref);
+        $confirmAction.attr("class", actionClass);
         $confirmAction.html(actionText);
     });
 })
