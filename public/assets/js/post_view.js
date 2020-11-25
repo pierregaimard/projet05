d13e8a2c8a8a2fa6639c463db6e21fc7f6b58b43
Pierre Gaimard <pierre@gaimard-family.fr>
2020-11-25 15:10:03 +0100

    Move post view script in external js file

---
+++ public/assets/js/post_view.js
@@ -0,0 +1,21 @@
+/*global hljs*/
+/*eslint no-undef: "error"*/
+
+hljs.initHighlightingOnLoad();
+$(document).ready(function () {
+    let $action = $('.app-comment-actions a');
+    let $confirmText = $('#post-comment-action-confirm .app-confirm-action');
+    let $confirmAction = $('#post-comment-action-confirm a');
+
+    $action.on('click', function () {
+        let text = $(this).data('text');
+        let actionText = $(this).data('action');
+        let actionHref = $(this).data('action-href');
+        let actionClass = $(this).data('action-class');
+
+        $confirmText.html(text);
+        $confirmAction.attr('href', actionHref);
+        $confirmAction.attr('class', actionClass);
+        $confirmAction.html(actionText);
+    });
+})
